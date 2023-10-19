eb0dd360ed000f7bbf46eb6e835060ef62d4ecc3
Mousin <officialmulani2@gmail.com>
2023-10-19 22:14:19 +0530

    first commit

---
+++ force-app/main/default/lwc/dummy/dummy.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Dummy extends LightningElement {}
---
+++ force-app/main/default/lwc/dummy2/dummy2.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Dummy2 extends LightningElement {}
